bdb77df062cb83c9efdbb2d1bf7232a30f338b8e
Cristian Cisternas <ccisternas@cuprum.cl>
2022-05-20 17:59:58 -0400

    first commit

---
+++ CCisternasMiPrimeraFuncionLambda/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCisternasMiPrimeraFuncionLambda.Controllers;
+
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+
+    //GET api/holamundo
+
+    [HttpGet]
+    [Route("[action]")]
+    public string HolaMundo()
+    {
+        return "Mi primera funcion lambda - Cristian Cisternas";
+    }
+
+    // GET api/values
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
